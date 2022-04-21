cd089800758d38e17698de6150e6ff720f39e2bf
Amelia <amelia.travers@gmail.com>
2022-04-21 22:19:14 +1000

    styling and links and logos done

--- src/components/faq/faq.jsx
+++ src/components/faq/faq.jsx
@@ -12,21 +12,23 @@ import FaqData from '../../data/faq';
 function Faq() {
   return (
     FaqData.map((faq) => (
-      <Accordion allowToggle>
-        <AccordionItem>
-          <h2>
-            <AccordionButton>
-              <Box flex="1" textAlign="left">
-                {faq.question}
-              </Box>
-              <AccordionIcon />
-            </AccordionButton>
-          </h2>
-          <AccordionPanel pb={2}>
-            {faq.answer}
-          </AccordionPanel>
-        </AccordionItem>
-      </Accordion>
+      <Box>
+        <Accordion allowToggle>
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box flex="1" textAlign="left">
+                  {faq.question}
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={2}>
+              {faq.answer}
+            </AccordionPanel>
+          </AccordionItem>
+        </Accordion>
+      </Box>
     ))
   );
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,7 @@
 import * as React from 'react';
+import {
+  Heading, Container, Text, Link, Flex, Image,
+} from '@chakra-ui/react';
 import Footer from '../components/footer/footer';
 import Faq from '../components/faq/faq';
 
@@ -11,28 +14,33 @@ function IndexPage() {
       <title>Home Page</title>
       <header>
 
-        <h1>
+        <Heading as="h1" textAlign="center" size="2xl" color="#F98E4A">
           #MyScienceMay
-        </h1>
-        <div>Their logo</div>
-        <div>My logo</div>
+        </Heading>
+        <Flex justify="center">
+          <Image src="/static/images/avidResearchLogo.png" alt="Avid Research Logo" boxSize={['120px', '150px', '200px']} fit="contain" margin={[3, 4, 5]} />
+          <Image src="/static/images/letsTalkScicommLogo.png" alt="Let's talk SciComm Logo" boxSize={['120px', '150px', '200px']} fit="contain" margin={[3, 4, 5]} />
+        </Flex>
       </header>
-
-      <p>
+      <Text textAlign="center" m="5">
         A social media challenge from
-        <em>Let&apos;s Talk Scicomm</em>
+        <Link href="https://anchor.fm/letstalkscicomm" color="#F98E4A" fontWeight="bold"> Let&apos;s Talk SciComm </Link>
         and
-        <em>Avid Research</em>
+        <Link href="https://avidresearch.com.au/" color="#F98E4A" fontWeight="bold"> Avid Research   </Link>
         to help you find connections and collaborations for your science
-      </p>
-      <p>
-        Join us on Twitter with the #MyScienceMay hashtag
-      </p>
-      <div>
-        <div>Scicomm Twitter</div>
-        <div>My Twitter</div>
-      </div>
-      <h2>Schedule</h2>
+      </Text>
+      <Text textAlign="center" m="5">
+        Join us on Twitter with the
+        <b> #MyScienceMay </b>
+        hashtag
+      </Text>
+      <Flex justify="center" direction={['column', 'row']} align="center">
+        <Link href="https://twitter.com/LetsTalkSciComm" color="#00B2A9" m="3" fontWeight="bold">@LetsTalkSciComm</Link>
+        <Link href="https://twitter.com/avid_podcast" color="#00B2A9" m="3" fontWeight="bold">@avid_podcast</Link>
+      </Flex>
+      <Heading as="h3" textAlign="center" fontWeight="bold" fontSize="xl" p="5">
+        Schedule
+      </Heading>
       <ol>
         <li>
           Monday
@@ -46,10 +54,12 @@ function IndexPage() {
       </ol>
       <div>Download the Schedule</div>
       <div>
-        <h3>
+        <Heading as="h3" textAlign="center" fontWeight="bold" fontSize="xl" p="5">
           FAQs
-        </h3>
-        <Faq />
+        </Heading>
+        <Container border="solid 1px #00B2A9" borderRadius="5px">
+          <Faq />
+        </Container>
       </div>
       <Footer />
     </main>
